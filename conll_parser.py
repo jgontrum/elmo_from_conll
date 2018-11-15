f351a28a347728c185196a121caced29048ebe07
jgontrum <johannes.gontrum@retresco.de>
2018-11-15 10:31:33 +0100

    Fix conllu package version

--- conll_parser.py
+++ conll_parser.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     args = cmdline_args()
 
     for token_list in parse_incr(open(args.conll_file)):
-        sentence = [token["form"].strip() for token in token_list]
+        sentence = [token["form"].strip() for token in token_list if len(token["form"])]
         if sentence:
             print(" ".join(sentence))
